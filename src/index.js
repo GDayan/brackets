eabf11959d06e53f1adf644113a7fd131e57b9e1
Mac <mac@MacBook-Pro-Mac.local>
2023-08-27 15:09:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const openingBrackets = [];
+  const closingBrackets = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openingBrackets.push(bracketsConfig[i][0]);
+    closingBrackets.push(bracketsConfig[i][1]);
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const currentBracket = str[i];
+
+    if (openingBrackets.includes(currentBracket)) {
+      // Если текущая скобка - открывающая, помещаем ее в стек
+      stack.push(currentBracket);
+    } else if (closingBrackets.includes(currentBracket)) {
+      // Если текущая скобка - закрывающая, проверяем соответствие с последней открывающей скобкой в стеке
+      const lastOpeningBracket = stack.pop();
+      const correspondingClosingBracket = closingBrackets[openingBrackets.indexOf(lastOpeningBracket)];
+
+      if (currentBracket !== correspondingClosingBracket) {
+        return false; // Несоответствие скобок
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
